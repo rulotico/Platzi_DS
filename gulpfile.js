7f461ba0c777908683c141b2fabdabd12cc7f136
Rulótico González <rulotico@Rulotico.local>
2018-11-08 17:23:10 -0500

    update: final

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var version= require('./package.json');
+
+//npm install gulp --save --only=dev
+gulp.task('hello', function() {
+  console.log('Hello ');
+});
+
+//npm install gulp-sass
+
+gulp.task('sass', function(){
+  return gulp.src('scss/**/*.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('public/stylesheets'))
+});
+
+//npm install gulp-concat
+
+gulp.task('style', function(){
+  return gulp.src('scss/**/*.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(concat('style_main.css'))
+    .pipe(gulp.dest('public/stylesheets'))
+});
+
+
+//npm install gulp-minify-css
+
+gulp.task('style_min', function(){
+  return gulp.src('scss/**/*.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(minifyCSS())
+    .pipe(concat('style_main_'+version.version+'.min.css'))
+    .pipe(gulp.dest('public/stylesheets'))
+});
+
+//gulp watch
+
+gulp.task('watch', ['style_min'], function (){
+  gulp.watch('scss/**/*.scss', ['style_min']);
+
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var version= require('../package.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' , version:version.version});
+
 });
+
+
+router.get('/Principios', function(req, res, next) {
+  res.render('principios', { title: 'Express' });
+});
+
+router.get('/Typo', function(req, res, next) {
+  res.render('typo', { title: 'Express' });
+});
+router.get('/Colors', function(req, res, next) {
+  res.render('colors', { title: 'Express' });
+});
+
+router.get('/grid', function(req, res, next) {
+  res.render('grid', { title: 'Express' });
+});
+router.get('/spacing', function(req, res, next) {
+  res.render('spacing', { title: 'Express' });
+});
+
+router.get('/animations', function(req, res, next) {
+  res.render('animation', { title: 'Express' });
+});
+
+//------------------------------------------------- COMPONENTES
+router.get('/navbar', function(req, res, next) {
+  res.render('components/navbar', { title: 'Express' });
+});
+
+
+
+router.get('/whats_new', function(req, res, next) {
+  res.render('news', { title: 'Express' , version:version.version});
+});
+
+
 
 module.exports = router;
